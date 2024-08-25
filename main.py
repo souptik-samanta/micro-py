f413cdd638092bb094a10f1ab7f5e5e1c8243eb6
Souptik Samanta <161435060+souptik-samanta@users.noreply.github.com>
2024-08-25 13:03:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ from machine import Pin
 import time
 
 # Initialize the onboard LED on GPIO 25
-led = Pin(25, Pin.OUT)
+led = Pin(19, Pin.OUT)
 
 # Blink the LED
 while True:
-    led.value(1)   # Turn on the LED
+    led.off()   # Turn on the LED
     time.sleep(1)  # Wait for 1 second
-    led.value(0)   # Turn off the LED
+    led.on()   # Turn off the LED
     time.sleep(1)  # Wait for 1 second
+
 
